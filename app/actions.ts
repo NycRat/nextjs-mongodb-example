82d25b3328a4c4e68ce7a18ecaf19af81306655a
Eric Xiao <ericxiao0000@gmail.com>
2024-08-15 21:53:57 -0700

    fixed static page generation when not wanted

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,13 @@
 import { wordsCollection } from "@/lib/mongodb";
 import { Word } from "@/lib/types";
 import { WithId } from "mongodb";
+import { unstable_noStore } from "next/cache";
 
 // defines server function, can be used in ssr component or server route
 export async function fetchWordsFromDB(): Promise<WithId<Word>[]> {
+  // stops SSG, because we know mongodb.find will change
+  unstable_noStore();
+
   let cursor = wordsCollection.find();
 
   let words: WithId<Word>[] = [];
